0b9f6874feec121917360690272a57042a4e8bf0
neiranie <email_anda@contoh.com>
2026-04-21 14:38:27 +0700

    Update

--- src/main/java/com/praktikumDB/deploy/controller/UserController.java
+++ src/main/java/com/praktikumDB/deploy/controller/UserController.java
@@ -1,4 +1,40 @@
 package com.praktikumDB.deploy.controller;
 
+import com.praktikumDB.deploy.service.UserService;
+import com.praktikumDB.deploy.model.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
 public class UserController {
+    private final UserService userService;
+
+    public userController(UserService userService) {
+        this.userService = UserService;
+    }
+
+    @PostMapping
+    public String createUser(@RequestBody User request){
+        return "user created successfully";
+    }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @DeleteMapping("/id")
+    public String deleteUser(@PathVariable String Id){
+        userService.deleteUser(id);
+        return "user deleted successfully";
+    }
+
+    @PutMapping("/id")
+    public String updateUser(@PathVariable String id, @RequestBody User request) {
+        userService.updateUser(id, request);
+        return "user updated succesfully";
+    }
 }
+
--- src/main/java/com/praktikumDB/deploy/service/UserService.java
+++ src/main/java/com/praktikumDB/deploy/service/UserService.java
@@ -1,4 +1,40 @@
 package com.praktikumDB.deploy.service;
 
+import com.praktikumDB.deploy.model.User;
+import com.praktikumDB.deploy.repository.UserRepository;
+import jakarta.validation.constraints.Null;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User addUser(User request){
+        request.setId(UUID.randomUUID().toString());
+        return userRepository.save(request);
+    }
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public User getUserById(String id){
+        return userRepository.findById(id).orElse(null);
+        if(existingUser != null){
+            existingUser.setName{request.getName());
+            existingUser.setNim{request.getNim());
+            return userRepository.save(exsistingUser);
+        }
+        return null;
+    }
+
+    public void deleteUser(String Id)
+            userRepository.deleteById();
 }
